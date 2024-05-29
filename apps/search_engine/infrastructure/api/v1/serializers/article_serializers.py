96854b21a0d4213180f747338b93d9b627fba5a8
JoffreC <joffre.condor@epn.edu.ec>
2024-05-29 15:37:39 -0500

    feat:add serializers

--- apps/search_engine/infrastructure/api/v1/serializers/article_serializers.py
+++ apps/search_engine/infrastructure/api/v1/serializers/article_serializers.py
@@ -3,4 +3,13 @@ from rest_framework import serializers
 
 class ArticleSerializer(serializers.Serializer):
     title = serializers.CharField()
+    abstract = serializers.CharField()
+    doi = serializers.CharField()
+    publication_date = serializers.CharField()
+    author_count = serializers.IntegerField()
+    affiliation_count = serializers.IntegerField()
+    corpus = serializers.CharField()
+    affiliations = serializers.SerializerMethodCharField()
+    topics = serializers.SerializerMethodCharField()
+
 
