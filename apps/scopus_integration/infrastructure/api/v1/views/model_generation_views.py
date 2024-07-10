1773ad6ff38b8df02ec070a33d0a8a0ce993a16a
Fernando Sangopanta <jhon.sangopanta@epn.edu.ec>
2024-07-10 00:07:32 -0500

    add: locker for instances of update authors and scopus integration

--- apps/scopus_integration/infrastructure/api/v1/views/model_generation_views.py
+++ apps/scopus_integration/infrastructure/api/v1/views/model_generation_views.py
@@ -11,7 +11,7 @@ class GenerateModelView(APIView):
             model_generator = ModelGenerationService()
             corpus = model_generator.read_path()
             model_generator.generate_model(corpus)
-            return Response({'status': 'success'},
+            return Response({'success': True},
                             status=status.HTTP_200_OK)
         except Exception as e:
-            return Response({'status': 'error', 'message': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'success': False, 'message': str(e)}, status=status.HTTP_400_BAD_REQUEST)
